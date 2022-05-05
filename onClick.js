7f8a3b765b2da99bb6f6c20915bb6a42b0b7ef0e
Leo Maza <leomaza2222@gmail.com>
2022-05-05 10:29:24 -0300

    .

---
+++ onClick.js
@@ -0,0 +1,10 @@
+$("#contrasenia_ingresada").keypress(function(event) {
+    if (event.keyCode === 13) {
+        $("#on_click").click();
+    }
+});
+$("#usuario_ingresado").keypress(function(event) {
+    if (event.keyCode === 13) {
+        $("#on_click").click();
+    }
+});
